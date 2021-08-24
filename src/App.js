8da2cf4d3d78cd6ea0f75d5493535ed0729cc7b6
Andrew Rusli <andreworusli@gmail.com>
2021-08-24 17:17:08 -0400

    add some basic components and states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Navbar from './Navbar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// use state to determine which nav item is selected.
+// then conditionally render correct component
+
+const App = () => {
+
+    const [work, setWork] = useState(true);
+    const [about, setAbout] = useState(false);
+
+    return (
+        <div>
+            <Navbar 
+            work={work}
+            about={about}
+            setWork={setWork}
+            setAbout={setAbout}
+            ></Navbar>
+            { work ? (<div>Work Content</div>) : (<div>About Content</div>) }
+        </div>
+    )
+
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = ({work, about, setWork, setAbout}) => {
+    
+    const onWorkClick = () => {
+        setWork(true);
+        setAbout(false);
+    };
+
+    const onAboutClick = () => {
+        setAbout(true);
+        setWork(false);
+    }
+
+    console.log(`work state is ${work}`)
+
+    return (
+        <div>
+            <h1>Piyali, Designer</h1>
+            <ul>
+                <li style={{cursor: 'pointer'}} onClick={onWorkClick}>Work</li>
+                <li style={{cursor: 'pointer'}} onClick={onAboutClick}>About</li>
+            </ul>
+        </div>
+    );
+       
+}
+
+export default Navbar;
