462c8866d88357b7875ec2c3b468b495d1453dd0
Andrew Rusli <andreworusli@gmail.com>
2021-08-25 07:46:26 -0400

    created 1to1 aspect ratio containers in css

--- src/Work.js
+++ src/Work.js
@@ -4,19 +4,44 @@ import React from 'react';
 //Need way to conditionally render the selected project. Should be self generating I think. 
 //Generate from an array download from contently?
 //might need to use .map method to create all the project listings with correlating onClick functions/array/objects.
-
+//Basically, all text and images (all content) should be organized as an array of objects before entering html.
 
 const Work = () => {
 
     return (
         <div>
             <div class='grid-container-work'>
-                <div class='project-square pointer'>Project 1</div>
-                <div class='project-square pointer'>Project 2</div>
-                <div class='project-square pointer'>Project 3</div>
-                <div class='project-square pointer'>Project 4</div>
-                <div class='project-square pointer'>Project 5</div>
-                <div class='project-square pointer'>Project 6</div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 1
+                    </div>
+                </div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 2
+                    </div>
+                </div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 3
+                    </div>
+                </div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 4
+                    </div>
+                </div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 5
+                    </div>
+                </div>
+                <div class='aspect-square pointer'>
+                    <div class='canvas center'>
+                        Project 6
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
