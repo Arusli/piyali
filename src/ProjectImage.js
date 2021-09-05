c25d29933667bd69175b38f218c3de7223e7c9ec
Andrew Rusli <andreworusli@gmail.com>
2021-09-05 15:54:27 -0400

    removed aspect square from project pictures

--- src/ProjectImage.js
+++ src/ProjectImage.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ProjectImage = ({itemsArray, selectedProject, index}) => {
     return (
-        <div style={{backgroundImage: `url(${itemsArray[selectedProject].fields.images[index].fields.file.url})`}} className='aspect-square project-pic'></div>
+        <div style={{backgroundImage: `url(${itemsArray[selectedProject].fields.images[index].fields.file.url})`}} 
+            className='project-pic'>
+        </div>
     );
 };
 
