30fa01fdf4c6a9f7e7866fd8be2031081b09994c
Andrew Rusli <andreworusli@gmail.com>
2021-09-06 10:08:01 -0400

    add highlighting logic/css for main navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Navbar = (
     {aboutview, 
@@ -9,16 +9,21 @@ const Navbar = (
     setInnerview,
     }) => {
     
+
+    const [workActive, setWorkActive] = useState(true);
+
     const onWorkClick = () => {
        setAboutview(false);
        setOverview(true);
        setInnerview(false);
+       setWorkActive(true);
     };
 
     const onAboutClick = () => {
         setAboutview(true);
         setInnerview(false);
         setOverview(false);
+        setWorkActive(false);
     }
 
     // console.log(`work state is ${work}`)
@@ -27,8 +32,8 @@ const Navbar = (
         <div>
             <ul className='flex-row navbar'>
                 <li className='title'>Piyali Sircar | Designer</li>
-                <li className='pointer navbar-item' onClick={onWorkClick}>Work</li>
-                <li className='pointer navbar-item' onClick={onAboutClick}>About</li>
+                <li className={workActive ? 'pointer navbar-item highlight' : 'pointer navbar-item'} onClick={onWorkClick}>Work</li>
+                <li className={!workActive ? 'pointer navbar-item highlight' : 'pointer navbar-item'} onClick={onAboutClick}>About</li>
             </ul>
         </div>
     );
